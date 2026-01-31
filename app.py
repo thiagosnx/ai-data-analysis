980358756116c9c18793f217df210eddf0767543
thiagosnx <thiagosantanacq@gmail.com>
2026-01-30 22:30:52 -0300

    feat: bigquery extract

--- app.py
+++ app.py
@@ -1,8 +1,53 @@
 import streamlit as st
+import pandas as pd
+import pandas_gbq as bq
 
-
-# from services.data_extract_service import DataExtractService 
+from services.data_extract_service import DataExtractService 
 from services.data_transformation_service import DataTransformationService 
 
-data = DataTransformationService()
-st.write(data.transformation_data_default())
+
+
+# file = st.file_uploader(
+#     "Escolha um arquivo",
+#     type=["csv", "parquet", "xlsx"]
+# )
+
+from google.cloud import bigquery
+
+client = bigquery.Client.from_service_account_json(
+    "config.json"
+)
+
+query = """
+    select distinct
+        nome, 
+        marca, 
+        string_agg(tamanho, ', ' order by tamanho) as tamanhos,
+        preco_atual, 
+        preco_real
+    from `eco-avenue-461519-f8.telegramdata.shops`
+    where 1=1
+    and preco_atual < preco_real
+    and disponivel = 1
+    group by 
+        nome, 
+        marca, 
+        preco_atual, 
+        preco_real
+    order by preco_atual asc
+"""
+
+query_job = client.query(query)
+rows = query_job.result()
+
+df = bq.read_gbq(
+    query,
+    project_id="eco-avenue-461519-f8",
+    dialect="standard"
+)
+
+
+# raw = DataExtractService(file)
+# data = DataTransformationService()
+# st.write(data.transformation_data_default(raw))
+st.write(df)
--- services/data_transformation_service.py
+++ services/data_transformation_service.py
@@ -4,16 +4,27 @@ from services.data_extract_service import DataExtractService
 
 class DataTransformationService:
     def transformation_data_default(self):
+
         dt = DataExtractService()
 
         df = dt.extract_data_default()
-
         df = (
             df \
             .assign(
-                nome=lambda x: x["nome"].str.title(),
+                nome=lambda x: x["nome"].str.split().str[0],
+                email=lambda x: x["email"].str.replace(
+                    r'(^.).*(@.*$)', 
+                    r'\1****\2', 
+                    regex=True
+                ),
+                matricula=lambda x: x["matricula"].str.replace(
+                    r'.*(\d{2})$',
+                    r'*******\1',
+                    regex=True
+                ),
                 modelo=lambda x: x["nome_modelo"].str.title(),
-                preco=lambda x: pd.to_numeric(x["preco"], errors="coerce")
+                estado=lambda x: x["estado"].str.title(), # if not uf
+                preco=lambda x: pd.to_numeric(x["preco"], errors="coerce"),
             )
         )
         return df
